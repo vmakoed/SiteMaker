3e560016fff98798df5c85dced0275d3d8df2cb1
Eugene <kunitsa.evgeniy@gmail.com>
2015-08-11 00:05:02 +0300

    Update Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => "isitemailer@gmail.com",
-      :password       => "ololo1996",
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
